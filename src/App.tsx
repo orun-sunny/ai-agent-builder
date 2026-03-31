dc09eeebfe0cc1707094ebe5c1552a83e84d6643
orun-sunny <sunny.mukut@gmail.com>
2026-03-31 14:20:10 +0600

    fix: remove stale analyticsHeartbeat useEffect (Bug 3)

--- src/App.tsx
+++ src/App.tsx
@@ -42,20 +42,6 @@ function App() {
     }
   }, [])
 
-  useEffect(() => {
-    const analyticsInterval = setInterval(() => {
-      if (agentName !== '') {
-        console.log(`[Analytics Heartbeat] User is working on agent named: "${agentName}"`)
-      } else {
-        console.log(`[Analytics Heartbeat] User is working on an unnamed agent draft...`)
-      }
-    }, 8000)
-
-    return () => clearInterval(analyticsInterval)
-  }, [])
-
-
-
   const handleLayerSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const layerId = e.target.value;
     if (layerId) {
